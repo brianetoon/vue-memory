204f6d4d5cdc1ca37af5aa279b4589b6519aac6f
Brian <brian.elliot.toon@gmail.com>
2023-05-22 16:42:24 -0400

    Updates

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-// global styles
-import './assets/global.css'
-
-createApp(App).use(router).mount('#app')
